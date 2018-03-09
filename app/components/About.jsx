c433f4eaf3016e1677667cd5cf1cd3aa2210b534
suvrat <suvrat12@gmail.com>
2018-03-10 01:39:17 +0530

    idea excption

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var About = React.createClass({
     render: function () {
         return(
-            <h2>About Component</h2>
+            <div>
+                <h2>About Component</h2>
+            </div>
+
         );
     }
 });
