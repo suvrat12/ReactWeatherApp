9538a4ba016163f8ea1959256ad049aa595f50b9
suvrat <suvrat12@gmail.com>
2018-03-11 23:48:51 +0530

    Phase 2.0

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,9 +1,22 @@
 var React = require('react');
+var {Link}= require('react-router');
 
 var Example = React.createClass({
     render: function () {
         return(
-            <h2>Example Component</h2>
+            <div>
+                <h1 className="text-center">Examples</h1>
+                <p>Here are few example location to try out:</p>
+                <ol>
+                    <li>
+                        <Link to="/?location=Mumbai">Mumbai, India</Link>
+                    </li>
+                    <li>
+                        <Link to="/?location=Delhi">Delhi, India</Link>
+                    </li>
+                </ol>
+            </div>
+
         );
     }
 });
