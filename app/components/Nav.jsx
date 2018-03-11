ad05dc803b7c61d512d61e61c302e0f2e68b7e39
suvrat <suvrat12@gmail.com>
2018-03-11 18:02:57 +0530

    Foundation and styling --Button

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,14 +2,39 @@ var React = require('react');
 var {Link,IndexLink}= require('react-router');
 
 var Nav = React.createClass({
+    onSearch:function(){
+      alert("Not wired");
+    },
    render: function () {
        return(
-           <div>
-               <h2>Nav Component!</h2>
-               <IndexLink to='/' activeClassName="active" activeStyle={{ fontWeight: 'bold'}}>Get Weather</IndexLink>
-               <Link to='/about' activeClassName="active" activeStyle={{ fontWeight: 'bold'}}>About</Link>
-               <Link to='/example' activeClassName="active" activeStyle={{ fontWeight: 'bold'}}>Example</Link>
-           </div>
+        <div className='top-bar'>
+            <div className='top-bar-left'>
+                <ul className="menu">
+                    <li className="menu-text">React Weather App</li>
+                    <li>
+                        <IndexLink to='/' activeClassName="active" activeStyle={{ fontWeight: 'bold'}}>Get Weather</IndexLink>
+                    </li>
+                    <li>
+                        <Link to='/about' activeClassName="active" activeStyle={{ fontWeight: 'bold'}}>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/example' activeClassName="active" activeStyle={{ fontWeight: 'bold'}}>Example</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <form onSubmit={this.onSearch}>
+                    <ul className="menu">
+                        <li>
+                            <input type="search" placeholder="Search Weather"></input>
+                        </li>
+                        <li>
+                            <input type="submit" className="button" value="Get Weather"></input>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
            
        );
    }
