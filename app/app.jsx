6a5142cdfd766252e8abe9f18f4a89e9cd6df414
suvrat <suvrat12@gmail.com>
2018-03-11 17:30:02 +0530

    Foundation and styling

--- app/app.jsx
+++ app/app.jsx
@@ -4,7 +4,10 @@ var {Route, Router, IndexRoute, hashHistory}= require('react-router');
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
-var Example= require('Example')
+var Example= require('Example');
+
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation()
 
 ReactDOM.render(
     <Router history={hashHistory}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+      'script!jquery/dist/jquery.min.js',
+      'script!foundation-sites/dist/foundation.min.js',
+      './app/app.jsx'],
+  externals:{
+      jquery:'jQuery'
+  },
+  plugins: [
+      new webpack.ProvidePlugin({
+          '$': 'jquery',
+          'jQuery': 'jquery'
+      })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -29,5 +43,6 @@ module.exports = {
         exclude: /(node_modules|bower_components)/
       }
     ]
-  }
+  },
+  devtool:'cheap-module-eval-source-map'
 };
